327f89d07d8bb9219cd3f8a7b44dbb601bb1a6c6
k777i <k777ig@gmail.com>
2016-12-16 21:07:37 +0200

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,20 @@
 public class Calculate {
 
-        public static void main(String[] args)  {
-                System.out.println("c");
+        public static void main(String[] arg)  {
+                System.out.println("Calculate ...");
+                double first = Double.valueOf(arg[0]);
+                double second = Double.valueOf(arg[1]);
+                double add = first + second;
+                double sub = first - second;
+                double mult = first * second;
+                double div = first / second;
+                double modul = first % second;
+
+                System.out.println("Sum = "+ add);
+                System.out.println("Sub = "+ sub);
+                System.out.println("Mult = "+ mult);
+                System.out.println("Div = "+ div);
+                System.out.println("Modul = "+ modul);
+
         }
 }
