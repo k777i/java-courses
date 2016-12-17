1b82b554aa86dcfbbadb21796aae99bd34529540
k777i <k777ig@gmail.com>
2016-12-17 12:13:45 +0200

    Calculate

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,35 @@
 public class Calculator {
-	
 	private int result;
 	
-	public void add(int ... params){
-		for(Integer param : params){
+	
+	public void add(int... params){
+		for(Integer param : params){       
 			this.result += param;
 			}
 	}
+			
+	public void subt(int params,int params1){  
+		this.result=params - params1;
+	}
+	
+	public void multipl(int params,int params1){
+		this.result=params * params1;
+	}				
+   
+	public void div(int params,int params1){
+		this.result=params / params1;
+	}
+	
+    public void mdl(int params,int params1){
+    	this.result=params % params1;
+	}
+    
+    public void sqr(int params,int params1){
+    	double d=Math.pow(params,params1);
+    	this.result = (int)d;
+    }
+	
+	
 	
 	public int getResult(){
 		return this.result;
--- InteractRunner.java
+++ InteractRunner.java
@@ -11,9 +11,20 @@ public class InteractRunner {
 			while(!exit.equals("yes")){
 				System.out.println("Enter first args :");
 				String first = reader.next();
+				System.out.println("Enter your arifmetic command :");
+				String command = reader.next();
 				System.out.println("Enter second args :");
 				String second = reader.next();
-				calc.add(Integer.valueOf(first),Integer.valueOf(second));
+			
+				switch(command){
+				case "+" : calc.add(Integer.valueOf(first),Integer.valueOf(second));break;
+				case "-" : calc.subt(Integer.valueOf(first),Integer.valueOf(second));break;
+				case "*" : calc.multipl(Integer.valueOf(first),Integer.valueOf(second));break;
+				case "/" : calc.div(Integer.valueOf(first),Integer.valueOf(second));break;
+				case "%" : calc.mdl(Integer.valueOf(first),Integer.valueOf(second));break;
+				case "sqr" : calc.sqr(Integer.valueOf(first),Integer.valueOf(second));break;
+				}
+				
 				System.out.println("Result : " + calc.getResult());
 				calc.cleanResult();
 				System.out.println("Exit : yes/no" );
